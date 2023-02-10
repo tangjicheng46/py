1fd30a1f5ab8589ef85df542ec9d51cb15e6a17e
tangjicheng <tangjicheng@sogou-inc.com>
2023-02-10 16:29:45 +0800

    测试面向对象的用法

--- source/oo_test.py
+++ source/oo_test.py
@@ -18,6 +18,24 @@ class D(B, C):
     pass
 
 
+def foo(x):
+    print(x.x)
+
+
+class E:
+    def __init__(self):
+        self.x = 1
+
+
+class F:
+    global_var = 10
+
+    def __init__(self):
+        pass
+
+
 if __name__ == '__main__':
     d = D()
     d.foo()
+    foo(E())
+    print(F.global_var)
