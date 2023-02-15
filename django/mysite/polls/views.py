9174eabb7a2b0ad9675010eb88342d13cd298e6f
tangjicheng <tangjicheng@sogou-inc.com>
2023-02-15 16:16:23 +0800

    添加新的视图函数

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Welcome first polls app!")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(f"Welcome first polls app! question is : \n{output}")
 
 
 def func(request):
     return HttpResponse("Hello, go to polls/")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s " % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s " % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting at %s " % question_id)
