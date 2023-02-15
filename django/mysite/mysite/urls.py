85a84b06faf0cdae7358155d352af7b4eca1862a
tangjicheng <tangjicheng@sogou-inc.com>
2023-02-15 13:42:08 +0800

    add app simple view

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import polls.views
 
 urlpatterns = [
+    path("", polls.views.func),
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Welcome first polls app!")
+
+
+def func(request):
+    return HttpResponse("Hello, go to polls/")
