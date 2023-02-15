b8a40a455912429bef0d9372d5e56239d69327f7
tang <tang@tencent.com>
2023-02-16 00:27:53 +0800

    在view中添加时间日期判断

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 from .models import Question
 
 
@@ -12,7 +14,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
