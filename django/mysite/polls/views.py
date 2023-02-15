e13f5d0769b0e541fab17332f4e1448f0fd225a8
tangjicheng <tangjicheng@sogou-inc.com>
2023-02-15 16:35:25 +0800

    添加index/detail模版

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+from django.http import Http404
 from .models import Question
 
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(f"Welcome first polls app! question is : \n{output}")
+
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(f"Welcome first polls app! question is : \n{output}")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request=request, template_name="polls/index.html", context=context)
 
 
 def func(request):
@@ -13,7 +22,12 @@ def func(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s " % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {"question": question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request=request, template_name="polls/detail.html", context=context)
 
 
 def results(request, question_id):
