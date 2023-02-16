d0af1dc2579c6a55221ffcf440cc111574d711e1
tangjicheng <tangjicheng@sogou-inc.com>
2023-02-16 11:10:55 +0800

    修改后台管理页面

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+
+admin.site.register(Question, QuestionAdmin)
